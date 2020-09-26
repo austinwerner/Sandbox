c2ccecc2d370412df6768497c4c25072460b431e
Austin Werner <r.austin.werner@gmail.com>
2020-09-26 16:43:13 -0500

    Add gesture library for drag and drop

--- app/src/main/java/android/werner/mysandbox/ui/main/MainAdapter.kt
+++ app/src/main/java/android/werner/mysandbox/ui/main/MainAdapter.kt
@@ -5,19 +5,31 @@ import android.view.View
 import android.view.ViewGroup
 import android.werner.mysandbox.R
 import android.werner.mysandbox.models.MainListObject
-import androidx.recyclerview.widget.RecyclerView
+import com.thesurix.gesturerecycler.GestureAdapter
+import com.thesurix.gesturerecycler.GestureViewHolder
 import kotlinx.android.synthetic.main.main_list.view.*
 
-class MainAdapter(private val myDataset: List<MainListObject>,
-    private val clickListener: (String) -> Unit) :
-    RecyclerView.Adapter<MainAdapter.MyViewHolder>() {
+class MainAdapter(private val clickListener: (String) -> Unit) :
+    GestureAdapter<MainListObject, GestureViewHolder<MainListObject>>() {
 
-    class MyViewHolder(val view: View) :
-        RecyclerView.ViewHolder(view)
+    class MyViewHolder(view: View) :
+        GestureViewHolder<MainListObject>(view) {
+        override fun canDrag(): Boolean {
+            return true
+        }
+
+        override fun canSwipe(): Boolean {
+            return true
+        }
+
+        override fun bind(item: MainListObject) {
+            itemView.tv_main_list_title.text = item.title
+        }
+    }
 
     // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(parent: ViewGroup,
-                                    viewType: Int): MyViewHolder {
+                                    viewType: Int): GestureViewHolder<MainListObject> {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.main_list, parent, false)
 
@@ -25,14 +37,18 @@ class MainAdapter(private val myDataset: List<MainListObject>,
     }
 
     // Replace the contents of a view (invoked by the layout manager)
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.view.tv_main_list_title.text = myDataset[position].title
-        holder.view.setOnClickListener{
-            clickListener.invoke(myDataset[position].id)
+    override fun onBindViewHolder(holder: GestureViewHolder<MainListObject>, position: Int) {
+        holder.itemView.tv_main_list_title.text = data[position].title
+        holder.itemView.setOnClickListener{
+            clickListener.invoke(data[position].id)
         }
     }
 
+    override fun getItemViewType(viewPosition: Int): Int {
+        return R.layout.main_list
+    }
+
     // Return the size of your dataset (invoked by the layout manager)
-    override fun getItemCount() = myDataset.size
+    override fun getItemCount() = data.size
 
 }
--- app/src/main/java/android/werner/mysandbox/ui/main/MainFragment.kt
+++ app/src/main/java/android/werner/mysandbox/ui/main/MainFragment.kt
@@ -7,19 +7,26 @@ import android.view.ViewGroup
 import android.werner.mysandbox.R
 import android.werner.mysandbox.Util.SNACKBAR_ID
 import android.werner.mysandbox.Util.TOAST_ID
+import android.werner.mysandbox.models.MainListObject
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import com.thesurix.gesturerecycler.DefaultItemClickListener
+import com.thesurix.gesturerecycler.GestureAdapter
+import com.thesurix.gesturerecycler.GestureManager
+import com.thesurix.gesturerecycler.RecyclerItemTouchListener
 import kotlinx.android.synthetic.main.main_fragment.*
 
 
 class MainFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
-    private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var gestureManager: GestureManager
 
 
     private val viewModel: MainViewModel by viewModels()
@@ -36,10 +43,11 @@ class MainFragment : Fragment() {
     private fun setUpRecyclerView() {
         viewManager = LinearLayoutManager(requireContext())
 
-        viewAdapter = MainAdapter(
-            viewModel.getData(),
+        val viewAdapter = MainAdapter(
             clickListener = { id -> handleItemClicked(id) }
         )
+
+        viewAdapter.data = viewModel.getData() as MutableList<MainListObject>
 
         recyclerView = rv_main_feed.apply {
             setHasFixedSize(true)
@@ -53,6 +61,40 @@ class MainFragment : Fragment() {
 
             adapter = viewAdapter
         }
+
+        recyclerView.addOnItemTouchListener(RecyclerItemTouchListener(object : DefaultItemClickListener<MainListObject>() {
+
+            override fun onItemClick(item: MainListObject, position: Int): Boolean {
+                Snackbar.make(view!!, "Click event on the $position position", Snackbar.LENGTH_SHORT).show()
+                return true
+            }
+
+            override fun onItemLongPress(item: MainListObject, position: Int) {
+                Snackbar.make(view!!, "Long press event on the $position position", Snackbar.LENGTH_SHORT).show()
+            }
+
+            override fun onDoubleTap(item: MainListObject, position: Int): Boolean {
+                Snackbar.make(view!!, "Double tap event on the $position position", Snackbar.LENGTH_SHORT).show()
+                return true
+            }
+        }))
+
+        gestureManager = GestureManager.Builder(recyclerView)
+            .setSwipeEnabled(true)
+            .setLongPressDragEnabled(true)
+            .setSwipeFlags(ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT)
+            .setDragFlags(ItemTouchHelper.UP or ItemTouchHelper.DOWN)
+            .build()
+
+        viewAdapter.setDataChangeListener(object : GestureAdapter.OnDataChangeListener<MainListObject> {
+            override fun onItemRemoved(item: MainListObject, position: Int, direction: Int) {
+                Snackbar.make(view!!, "Month removed from position $position ", Snackbar.LENGTH_SHORT).show()
+            }
+
+            override fun onItemReorder(item: MainListObject, fromPos: Int, toPos: Int) {
+                Snackbar.make(view!!, "Month moved from position $fromPos to $toPos", Snackbar.LENGTH_SHORT).show()
+            }
+        })
     }
 
     private fun handleItemClicked(id: String) {
